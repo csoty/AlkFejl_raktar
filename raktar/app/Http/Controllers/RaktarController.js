914535db17874ac05e6832e48fb1a58408f5781f
csoty <csoty95@gmail.com>
2016-11-19 23:55:53 +0100

    Index rendering changes

--- raktar/app/Http/Controllers/RaktarController.js
+++ raktar/app/Http/Controllers/RaktarController.js
@@ -1,7 +1,11 @@
 'use strict'
 
-class RaktarController {
+const Database = use('Database')
 
+class RaktarController {
+    * index (request, response) {
+        yield response.sendView('welcome');
+    }
 }
 
 module.exports = RaktarController;
